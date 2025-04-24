cb263bf092d9cdb7d84f4eae0e75906e5d7c58ec
hannahpruett <hpruett5@ivytech.edu>
2025-04-24 11:08:38 -0400

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
+from django.template import loader
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 
-def post_list(request):
+def post_list(request) -> HttpResponse:
+    template = loader.get_template('post_list.html')
     posts = Post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
